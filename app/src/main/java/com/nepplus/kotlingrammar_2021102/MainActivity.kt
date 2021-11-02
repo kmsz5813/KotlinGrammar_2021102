f7927b09d98b8a4d1346f528b1c6d139430d5ea6
김민성 <kmsz5813@naver.com>
2021-11-02 14:40:37 +0900

    변수의 기본 형태 연습  - 변수. toString() 으로 String 으로 변신가능

--- app/src/main/java/com/nepplus/kotlingrammar_2021102/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlingrammar_2021102/MainActivity.kt
@@ -2,10 +2,31 @@ package com.nepplus.kotlingrammar_2021102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnVariable01.setOnClickListener {
+//            기본적인 변수 만들고 / 대입하고 / 활용 (로그찍기)
+
+            val myName : String
+            var myHeigh : Double
+//           2. 변수에 데이터 대입
+
+            myName = "김민성"
+            myHeigh = "173.2"
+
+//            3. 데이터 활용
+            Log.d("이름", myName)
+            Log.d("키", myHeigh.toString())
+
+//            참고 - 모든 변수는 참조형변수, 부가 기능들이 있다. => 공통 기능 : String으로 변환 가능. => tostring
+
+        }
+
     }
 }
